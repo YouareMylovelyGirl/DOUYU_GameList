398423ed8fb4ba3d7ecc1fa0e614216662740aa9
阳光 <koreyoshi@yangguangdeMacBook-Pro.local>
2017-01-07 20:01:44 +0800

    创建了项目, 搭建好项目结构.

---
+++ DOUYU_GameList/DOUYU_GameList/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DOUYU_GameList
+//
+//  Created by 阳光 on 2017/1/7.
+//  Copyright © 2017年 YG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DOUYU_GameList/DOUYU_GameList/ViewController/GameListViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameListViewController.h
+//  DOUYU_GameList
+//
+//  Created by 阳光 on 2017/1/7.
+//  Copyright © 2017年 YG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameListViewController : UITableViewController
+
+@end
